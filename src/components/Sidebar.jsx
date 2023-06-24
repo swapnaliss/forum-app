c599d29d2c9f4186d0c7c5ebce0d25c54d026fca
swapnaliss <swapnalisbhargude123@gmail.com>
2023-06-24 21:40:46 +0530

    feature : UI changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
+import { AiOutlineHome, AiOutlineSearch, AiOutlineBook, AiOutlineUser } from 'react-icons/ai';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <Nav defaultActiveKey="/home" className="flex-column">
-        <Nav.Link disabled>Home</Nav.Link>
-        <Nav.Link disabled>Explore</Nav.Link>
-        <Nav.Link disabled>Bookmarks</Nav.Link>
-        <Nav.Link disabled>Profile</Nav.Link>
+        <Nav.Link disabled>
+          <AiOutlineHome className="nav-icon" color='blue' />
+          <span style={{ "color": "blue" }}>
+            Home
+          </span>
+        </Nav.Link>
+        <Nav.Link disabled>
+          <AiOutlineSearch className="nav-icon" />
+          Explore
+        </Nav.Link>
+        <Nav.Link disabled>
+          <AiOutlineBook className="nav-icon" />
+          Bookmarks
+        </Nav.Link>
+        <Nav.Link disabled>
+          <AiOutlineUser className="nav-icon" />
+          Profile
+        </Nav.Link>
       </Nav>
     </div>
   );
